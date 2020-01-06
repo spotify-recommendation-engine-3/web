9107e948235a73c3ccb8d057d9079a726057f15f
Kwang Woo Lee <kl467@cornell.edu>
2020-01-07 04:25:13 +0900

    login MVP added

--- symphinity/src/App.js
+++ symphinity/src/App.js
@@ -1,14 +1,27 @@
+// Import dependencies and general
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
+import './App.css'
+
+// Import components
+import Login from './components/login/Login';
+import Register from './components/registration/Register';
 import PrivateRoute from './components/auth/PrivateRoute';
 import SongSearch from './components/SongSearch';
-import './App.css'
 
 function App() {
   return (
     <>
       {/* <Route exact to="/login" component={Login} /> */}
-      <PrivateRoute exact to="/song-search" component={SongSearch} />
+      {/* <PrivateRoute exact to="/song-search" component={SongSearch} /> */}
+      <Switch>
+        <Route path="/">
+          <Login />
+        </Route>
+        <Route path="/register">
+          <Register />
+        </Route>
+      </Switch>
     </>
   );
 }
